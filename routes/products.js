8dd324196226ea24259b6442e2301d47dea33918
mcaggiano <caggianomauricio@gmail.com>
2021-09-03 17:04:36 -0300

    ----

--- routes/products.js
+++ routes/products.js
@@ -21,6 +21,9 @@ router.get("/create", productsController.create)
 
 router.post("/create", upload.single("img"),productsController.store);
 
+router.get('/:id/', productsController.detail);
 
+router.get('/:id/edit', productsController.edit);
+router.put('/:id/edit', productsController.update);
 
 module.exports = router
