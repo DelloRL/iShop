2b3a9edabc0e78a88f386516919cf1922fc307df
mcaggiano <caggianomauricio@gmail.com>
2021-12-03 18:41:22 -0300

    Products api, URLS

--- controllers/api/productsController.js
+++ controllers/api/productsController.js
@@ -6,9 +6,19 @@ const controller = {
     list: function(req, res)  {
         Product.findAll()
             .then(products => {
+
+                let productsWithUrl = [];
+                        products.forEach(product => {
+                            let newProduct = {
+                                ...product.dataValues,
+                                detail: `/api/products/${product.id}`
+                            }
+                            productsWithUrl.push(newProduct);            
+                        });
+
                 let response = {
                     count: products.length,
-                    products: products
+                    products: productsWithUrl
                 }
                 res.json(response)
             });
