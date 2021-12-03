5f7840537316ebebd7f4fa8ae57176b05f88eade
DelloRL <lucas.dello.russo@gmail.com>
2021-12-03 17:20:33 -0300

    '

--- middlewares/rememberMiddleware.js
+++ middlewares/rememberMiddleware.js
@@ -2,9 +2,9 @@ const db = require('../database/models/index.js');
 
 function remember(req, res, next) {
 
-    if (req.cookies.remember != undefined && req.session.userLogged == undefined) {
+    if (req.cookies.rememberMe != undefined && req.session.userLogged == undefined) {
 
-        db.users.findByPk(req.cookies.remember)
+        db.users.findByPk(req.cookies.rememberMe)
             .then(user => {
                 req.session.userLogged = user;
                 next();
