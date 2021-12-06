937ecb39367d8dbc314bdc78ddcfa088296d10e3
FranMazu <subzerov39@gmail.com>
2021-12-06 15:54:33 -0300

    solucion Api

--- controllers/api/productsController.js
+++ controllers/api/productsController.js
@@ -6,42 +6,39 @@ const controller = {
     list: function(req, res)  {
         Product.findAll()
             .then(products => {
-                
+
                 let productsWithUrl = [];
                         products.forEach(product => {
                             let newProduct = {
                                 ...product.dataValues,
-                                detail: `/api/products/${product.id}` 
+                                detail: `/api/products/${product.id}`
                             }
 
-                            productsWithUrl.push(newProduct);            
+                            productsWithUrl.push(newProduct);
                         });
-                
+
                 const lastProduct = products.sort(function(a,b){
-           
+
                     return new Date(b.created_at) - new Date(a.created_at);
                 }) [0];
-                
-                
+
+                let iphoneCategory =  products.filter(product => product.category === "IPhones").length
+                let auricularesCategory = products.filter(product => product.category === "Auriculares").length
+                let fundasCategory = products.filter(product => product.category === "Fundas y protección").length
 
                 let response = {
                     count: products.length,
                     products: productsWithUrl,
                     lastProduct,
-                    countByCategory: {
-                        iphones: {
-                            count: products.filter(product => product.category === "IPhones").length
-                        },
-                        auriculares: {
-                            count: products.filter(product => product.category === "Auriculares").length
-                        },
-                        fundas: {
-                            count: products.filter(product => product.category === "Fundas y protección").length
-                        }
+                    categorys: {
+                        iphoneCategory,
+                        auricularesCategory,
+                        fundasCategory
                     }
                 }
                 res.json(response)
             });
+        
     },
     detail: (req, res) => {
         Product.findByPk(req.params.id,)
